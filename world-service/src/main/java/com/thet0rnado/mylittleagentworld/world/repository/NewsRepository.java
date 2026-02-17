a1c06326f2d274412d6cb60facc3cfbd3df0413d
Ivan <Ivan22davydov@gmail.com>
2026-02-17 15:53:07 +0300

    World-service was changed and working well

---
+++ world-service/src/main/java/com/thet0rnado/mylittleagentworld/world/repository/NewsRepository.java
@@ -0,0 +1,16 @@
+package com.thet0rnado.mylittleagentworld.world.repository;
+
+import com.thet0rnado.mylittleagentworld.world.entity.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+    List<News> findByStatus(News.NewsStatus status);
+
+    List<News> findBySourceOrderByPublishedAtDesc(String source);
+
+}
