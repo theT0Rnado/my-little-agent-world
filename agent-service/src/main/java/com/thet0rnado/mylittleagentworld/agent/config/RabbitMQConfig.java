2a787a8881be03ff7b021f3a802d5b7b183415b9
1kawdalg <1kawdalg@gmail.com>
2026-02-18 00:58:30 +0300

    Adapted RabbitMQConfig

--- agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/config/RabbitMQConfig.java
+++ agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/config/RabbitMQConfig.java
@@ -12,33 +12,125 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class RabbitMQConfig {
 
-    @Value("${rabbitmq.exchange.requests}")
-    private String requestsExchange;
+    @Value("${rabbitmq.exchange.agent}")
+    private String agentExchange;
 
-    @Value("${rabbitmq.queue.requests-to-ai}")
-    private String requestsToAiQueue;
+    @Value("${rabbitmq.exchange.news}")
+    private String newsExchange;
 
-    @Value("${rabbitmq.routing-key.requests-to-ai}")
-    private String requestsToAiRoutingKey;
+    @Value("${rabbitmq.queue.agent.all.agent.data}")
+    private String agentAllAgentDataQueue;
+
+    @Value("${rabbitmq.queue.agent.message}")
+    private String agentMessageQueue;
+
+    @Value("${rabbitmq.queue.agent.all.agent.data.from.ai}")
+    private String agentAllAgentDataFromAiQueue;
+
+    @Value("${rabbitmq.queue.agent.message.from.ai}")
+    private String agentMessageFromAiQueue;
+
+    @Value("${rabbitmq.queue.news.from.ai}")
+    private String newsFromAiQueue;
+
+    @Value("${rabbitmq.routing-key.agent.all.agent.data}")
+    private String agentAllAgentDataRoutingKey;
+
+    @Value("${rabbitmq.routing-key.agent.message}")
+    private String agentMessageRoutingKey;
+
+    @Value("${rabbitmq.routing-key.agent.all.agent.data.from.ai}")
+    private String agentAllAgentDataFromAiRoutingKey;
+
+    @Value("${rabbitmq.routing-key.agent.message.from.ai}")
+    private String agentMessageFromAiRoutingKey;
+
+    @Value("${rabbitmq.routing-key.news.from.ai}")
+    private String newsFromAiRoutingKey;
 
     @Bean
-    public TopicExchange requestsExchange() {
-        return new TopicExchange(requestsExchange);
+    public TopicExchange agentExchange() {
+        return new TopicExchange(agentExchange);
     }
 
     @Bean
-    public Queue requestsToAiQueue() {
+    public TopicExchange newsExchange() {
+        return new TopicExchange(newsExchange);
+    }
+
+    @Bean
+    public Queue agentAllAgentDataQueue() {
         return QueueBuilder
-                .durable(requestsToAiQueue)
+                .durable(agentAllAgentDataQueue)
                 .build();
     }
 
     @Bean
-    public Binding newsToAiBinding(Queue requestsToAiQueue, TopicExchange requestsExchange) {
+    public Queue agentMessageQueue() {
+        return QueueBuilder
+                .durable(agentMessageQueue)
+                .build();
+    }
+
+    @Bean
+    public Queue agentAllAgentDataFromAiQueue() {
+        return QueueBuilder
+                .durable(agentAllAgentDataFromAiQueue)
+                .build();
+    }
+
+    @Bean
+    public Queue agentMessageFromAiQueue() {
+        return QueueBuilder
+                .durable(agentMessageFromAiQueue)
+                .build();
+    }
+
+    @Bean
+    public Queue newsFromAiQueue() {
+        return QueueBuilder
+                .durable(newsFromAiQueue)
+                .build();
+    }
+
+    @Bean
+    public Binding agentAllAgentDataBinding(Queue agentAllAgentDataQueue, TopicExchange agentExchange) {
         return BindingBuilder
-                .bind(requestsToAiQueue)
-                .to(requestsExchange)
-                .with(requestsToAiRoutingKey);
+                .bind(agentAllAgentDataQueue)
+                .to(agentExchange)
+                .with(agentAllAgentDataRoutingKey);
+    }
+
+    @Bean
+    public Binding agentMessageBinding(Queue agentMessageQueue, TopicExchange agentExchange) {
+        return BindingBuilder
+                .bind(agentMessageQueue)
+                .to(agentExchange)
+                .with(agentMessageRoutingKey);
+    }
+
+    @Bean
+    public Binding agentAllAgentDataFromAiBinding(Queue agentAllAgentDataFromAiQueue, TopicExchange agentExchange) {
+        return BindingBuilder
+                .bind(agentAllAgentDataFromAiQueue)
+                .to(agentExchange)
+                .with(agentAllAgentDataFromAiRoutingKey);
+    }
+
+    @Bean
+    public Binding agentMessageFromAiBinding(Queue agentMessageFromAiQueue, TopicExchange agentExchange) {
+        return BindingBuilder
+                .bind(agentMessageFromAiQueue)
+                .to(agentExchange)
+                .with(agentMessageFromAiRoutingKey);
+    }
+
+    @Bean
+    public Binding newsFromAiBinding(Queue newsFromAiQueue, TopicExchange newsExchange) {
+        return BindingBuilder
+                .bind(newsFromAiQueue)
+                .to(newsExchange)
+                .with(newsFromAiRoutingKey);
     }
 
     @Bean
