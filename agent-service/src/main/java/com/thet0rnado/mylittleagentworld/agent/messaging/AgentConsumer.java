d29e0120cd891272d33b2cb1b1b80072930bb013
1kawdalg <1kawdalg@gmail.com>
2026-02-18 02:42:38 +0300

    Adapted agent-service AgentConsumer

--- agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/messaging/AgentConsumer.java
+++ agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/messaging/AgentConsumer.java
@@ -1,6 +1,8 @@
 package com.thet0rnado.mylittleagentworld.agent.messaging;
 
 import com.thet0rnado.mylittleagentworld.agent.dto.AgentListFromAiMessage;
+import com.thet0rnado.mylittleagentworld.agent.dto.AgentMessageTextMessage;
+import com.thet0rnado.mylittleagentworld.agent.service.AgentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,14 +13,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AgentConsumer {
 
+    private AgentService agentService;
+    private AgentProducer agentProducer;
+
     @RabbitListener(queues = "${rabbitmq.queue.agent-all-agent-data-from-ai}")
     private void startCreatingAgents(AgentListFromAiMessage message) {
 
     }
 
-//    @RabbitListener(queues = "${rabbitmq.queue.agent-message-from-ai}")
-//    private void startCreatingAgents( message) {
-//
-//    }
+    @RabbitListener(queues = "${rabbitmq.queue.agent-message-from-ai}")
+    private void setMessageToAgent(AgentMessageTextMessage message) {
+
+    }
 
 }
