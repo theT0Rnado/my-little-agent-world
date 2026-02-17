20097da9d789f4745f9b5762d4df84d1a7c06ef8
1kawdalg <1kawdalg@gmail.com>
2026-02-17 12:44:00 +0300

    Main config is ended

--- agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/AgentServiceApplication.java
+++ agent-service/src/main/java/com/thet0rnado/mylittleagentworld/agent/AgentServiceApplication.java
@@ -2,8 +2,10 @@ package com.thet0rnado.mylittleagentworld.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AgentServiceApplication {
 
 	public static void main(String[] args) {
